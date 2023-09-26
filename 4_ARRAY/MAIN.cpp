a3bf6150a8bdccfb24a6dc47b4ece03e2c53c09f
CS <chansol930@naver.com>
2023-09-26 13:14:32 +0900

    배열 공부 다음이 포인터인데 나 잘 할 수 있을까? Signed-off-by: Q <max0621@naver.com>

--- 4_ARRAY/MAIN.cpp
+++ 4_ARRAY/MAIN.cpp
@@ -23,6 +23,78 @@
 
 #include "calcfunc.h"
 
+int ADD(int a, int b)
+{
+	printf("int ADD가 사용\n");
+	return a + b;
+}
+
+float ADD2(float a, float b)
+{
+	printf("float ADD가 사용\n");
+	return a + b;
+}
+
+double ADD3(double a, double b)
+{
+	printf("doble ADD가 사용\n");
+	return a + b;
+}
+
+struct fruitshop
+//struct는 구조체고 구조체는 int main 바깥에다가 만들어야 오류X
+//구조체 내부 선언한 변수는 구조체가 사라지지 않으면 사라지지 않는다.
+{
+	int appleA;
+	int mangoA;
+	int pearA;
+
+	//구조체 생성자 ":" 구문 오른쪽에 변수에 초기값을 셋팅한다.
+	fruitshop() : appleA(1000), mangoA(1000), pearA(1000)
+	{
+
+	}
+	//구조체에 함수 생성 -> 구조체에는 함수를 만들수 있다.
+	void setfruitvalue(int apple, int mango, int pear)
+	{
+		int appleB = apple; // setfruitvalue 함수가 종료되면 사라진다.
+		appleA = apple;
+		mangoA = mango;
+		pearA = pear;
+	}
+	//구조체에 출력하는 함수를 생성하면 편리하게 이용할 수있다.
+	void printshopvalue()
+	{
+		printf("과일가게 가격 사과:%d, 망고:%d, 배:%d\n", appleA, mangoA, pearA);
+	}
+};
+struct Phoneshop
+{
+	int iphone; int zflip; int s23; int fold;
+	Phoneshop() : iphone(100), zflip(100), s23(100), fold(100)
+	{}
+	void setPhonevalue(int niphone, int nzflip, int ns23, int nfold)
+	{
+		
+		iphone = niphone;
+		zflip = nzflip; 
+		s23 = ns23; 
+		fold = nfold;
+	}
+	void printShopvalue()
+	{
+		printf("우리동네 성지 가격 아이폰: %d, 제트플립 : %d, s23 : %d, 폴드 : %d\n", iphone, zflip, s23, fold
+		);
+	}
+
+
+};
+
+
+
+
+
+
 int main()
 {
 	//int t_Money = 0; // 지역변수 특정 함수 내에서만 적용되는 변수
@@ -31,18 +103,17 @@ int main()
 	//addMoney(100);
 	//addMoney(100);
 	//addMoney(100); // 링크업 예제// 링크업 실습
-	
+
 	/*printf("main t_Money=%d\n", t_Money);
 	printf("main g_Money=%d\n", g_Money);*/ // 링크업 실습
 
-
-	bool bRun = true;
-	while (bRun)
+	/*bool bRun = true;*/
+	/*while (bRun)
 	{
 		int nlput = 0;
 		int num1, num2;
 
-		printf("어떤 연산을 하시겠습니까? 0: +,2: *, 3: /, 4: % ): ");
+		printf(("어떤 연산을 하시겠습니까? 0: +,2: *, 3: /, 4: % "));
 		scanf_s("%d", &nlput);
 
 		switch (nlput)
@@ -74,7 +145,7 @@ int main()
 		{
 			printf("숫자 2개를 입력(num1 num2)");
 			scanf_s("%d %d", &num1, &num2);
-			printf("두수의 분은 %d\n", divide(num1, num2));
+			printf("두수의 분은 %e\n", divide(num1, num2));
 		}
 		break;
 
@@ -85,7 +156,7 @@ int main()
 			printf("두수의 나머지는 %d\n", remain(num1, num2));
 		}
 		break;
-	}
+		}
 
 
 
@@ -98,13 +169,64 @@ int main()
 			bRun = false;
 			break;
 		}
+	//}
+	/*ADD(1);
+	ADD2(1.0f, 1.0f); //f를 붙이고 실수를 쓰면 플롯형
+	ADD3(1.0, 1.0);*/ //f를 안붙이고 실수를 쓰면 더블형
 
+	// 배열
+	// int arrInt[10] = {0}; // 전부 0으로 초기화
+	// int arrInt[10] = {0 ,}; // 전부 0으로 초기화
+	int arrInt[10] = { 0, 1 , 2 , 3 , 4,  5 , 6 , 7 , 8 , 9 };//배열 생성 및 각각에 배열에 초기값 넣는 방법
+	arrInt[0] = 10; //배열 1번째 값에 넣기
+	arrInt[4] = 40; //배열 5번째에 값에 넣기
+	arrInt[9] = 90;// 0번 부터 배열이 시작되므로 10번째 배열은 9번
 
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d번째 배열 값은 %d입니다.\n", i, arrInt[ i ]); // 2개 이상의 파라미터 출력법
+	}
 
+	int number = 0;
+	// for each 구문: 배열에서만 사용이 가능하고, 배열 갯수를 계속 입력해주는 것이 귀찮기에.
+	//                특정 배열만 계속 사용하면 경제성이 떨어지기에 배열 값의 크기만 돌리는 것이다.
+	for (int value : arrInt) 
+		                    
+	{
+		printf("%d번째 배열 값은 %d입니다.\n", number, value);
+		number++;
+	}
+
+	
+	fruitshop shop[3];
+	// sturct에 사용
+	// fruitshop : 변수형
+	// shop : 변수의 이름
+	// 변수의 이름 옆에.을 찍어야 구조체 사용할수 있다.  
+	//shop.appleA = 1000; //shop에 appleA 변수에 값 넣기
+	//shop.mangoA = 2000;
+	//shop.pearA = 3000;
+	shop[0].setfruitvalue(1000, 2000, 3000);
+	shop[1].setfruitvalue(1100, 2100, 3100);
+	shop[2].setfruitvalue(900, 1900, 2900);
+	
+	for (fruitshop stvalue : shop)
+	{
+		stvalue.printshopvalue();
+	}
+
+	Phoneshop phoneshop[3];
+	phoneshop[0].setPhonevalue(100, 90, 80, 150);
+	phoneshop[1].setPhonevalue(110, 100, 90, 160);
+	phoneshop[2].setPhonevalue(90, 80, 70, 140);
+
+	for ( Phoneshop Stvalue : phoneshop)
+	{
+		Stvalue.printShopvalue();
 	}
 
 
-
+	
 
 
 
@@ -112,3 +234,6 @@ int main()
 
 
 }
+
+
+		
